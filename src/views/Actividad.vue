402530b2e89989df199ba1374c52c7e5e43a45f2
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2023-09-29 21:15:44 -0500

    Vinculación AD

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -10,14 +10,14 @@
             img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Nombre de la actividad
-        p.mb-4 #[b Objetivo] 
+          h2 Identificación de Conceptos Clave y Estrategias de Mejora
+        p.mb-4 Identificar y comprender los conceptos clave relacionados con la evaluación y las acciones de mejora en eventos deportivos para optimizar su calidad y eficiencia.
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Tipo de actividad
+              p.fw-bold.mb-0 Relacionar términos
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/index.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
