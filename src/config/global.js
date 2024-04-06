d691cff90dff622424373123c71a7455d2364719
Oscar Espitia <odespitia@sena.edu.co>
2024-04-06 12:57:35 -0500

    ajustes lista de chequeo

--- src/config/global.js
+++ src/config/global.js
@@ -127,7 +127,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CFA_08_122112.zip',
       },
       {
         icono: 'fas fa-download',
